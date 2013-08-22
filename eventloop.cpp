4d2b32aea15ab09c8d1884a361300a232741b9cd
Ilya Basin <basinilya@gmail.com>
2013-08-22 18:14:58 +0400

    x

--- eventloop.cpp
+++ eventloop.cpp
@@ -39,8 +39,14 @@ You can free the data structure immediately, the handler function won't be calle
 
 #include <windows.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef int (*evloop_func_t)(void *param);
 
+/** init */
+int evloop_init();
 /** add event listener */
 HANDLE evloop_addlistener(evloop_func_t func, void *param);
 /** remove event listener */
@@ -48,40 +54,147 @@ int evloop_removelistener(HANDLE ev);
 /** wait for events and process next event */
 int evloop_processnext();
 
+#ifdef __cplusplus
+}
+#endif
+
+#include <stdio.h>
 #include <vector>
-#include <string>
-#include <map>
 
 using namespace std;
 
 typedef struct evloop_handler {
+	LONG refcount;
 	evloop_func_t func;
 	void *param;
+	HANDLE hEvent;
 } evloop_handler;
 
 static struct {
-	basic_string<HANDLE> events;
-} ctx;
+	volatile HANDLE controlEvent;
+	CRITICAL_SECTION critsec;
+	vector<evloop_handler> handlers;
+} ctx = { NULL };
 
-//static 
-//static 
-
-static void _evloop_notifythreads()
-{
-}
+static void _evloop_notifyall();
+static void _evloop_release_handler(vector<evloop_handler>::iterator &_Where);
+static void _evloop_release_control_event(HANDLE ev);
+static HANDLE _evloop_acquire_control_event();
 
 HANDLE evloop_addlistener(evloop_func_t func, void *param)
 {
 	evloop_handler handler;
 	HANDLE newev;
 
-	newev = CreateEvent(NULL, FALSE, FALSE, NULL);
-
+	handler.hEvent = newev = CreateEvent(NULL, FALSE, FALSE, NULL);
+	handler.refcount = 1;
 	handler.func = func;
 	handler.param = param;
 
-	ctx.events += newev;
-	//GetCurrentThreadId
-	_evloop_notifythreads();
+	EnterCriticalSection(&ctx.critsec);
+	if (ctx.handlers.size() < MAXIMUM_WAIT_OBJECTS-1) {
+		ctx.handlers.push_back(handler);
+	} else {
+		printf("too many events\n");
+		CloseHandle(newev);
+		newev = NULL;
+	}
+	LeaveCriticalSection(&ctx.critsec);
+
+	_evloop_notifyall();
 	return newev;
+}
+
+int evloop_removelistener(HANDLE ev)
+{
+	EnterCriticalSection(&ctx.critsec);
+	for (vector<evloop_handler>::iterator it = ctx.handlers.begin(); it != ctx.handlers.end(); it++) {
+		if (it->hEvent == ev) {
+			it->func = NULL;
+			_evloop_release_handler(it);
+			break;
+		}
+	}
+	LeaveCriticalSection(&ctx.critsec);
+	return 0;
+}
+
+int evloop_processnext()
+{
+	HANDLE ev, nextev;
+	HANDLE handles[MAXIMUM_WAIT_OBJECTS];
+	DWORD nCount;
+	DWORD dwrslt;
+
+	handles[0] = ev = _evloop_acquire_control_event();
+	nCount = 1;
+
+	nextev = InterlockedExchangePointer(&ctx.controlEvent, ev);
+
+	EnterCriticalSection(&ctx.critsec);
+	for (vector<evloop_handler>::iterator it = ctx.handlers.begin(); it != ctx.handlers.end(); it++) {
+		if (it->func) {
+			it->refcount++;
+			handles[nCount++] = it->hEvent;
+		}
+	}
+	LeaveCriticalSection(&ctx.critsec); /* If handlers array changes after this, we'll be notified */
+
+	dwrslt = WaitForMultipleObjects(nCount, handles, FALSE, INFINITE);
+	_evloop_release_control_event(ev);
+
+	EnterCriticalSection(&ctx.critsec);
+	dwrslt -= WAIT_OBJECT_0;
+	switch(dwrslt) {
+		case 0:
+			if (nextev) SetEvent(nextev);
+			break;
+		default:
+			(&ctx.handlers.front())[dwrslt];
+			;
+	}
+	LeaveCriticalSection(&ctx.critsec);
+
+	EnterCriticalSection(&ctx.critsec);
+	for (vector<evloop_handler>::iterator it = ctx.handlers.begin(); it != ctx.handlers.end(); it++) {
+		_evloop_release_handler(it);
+		break;
+	}
+	LeaveCriticalSection(&ctx.critsec);
+
+	return 0;
+}
+
+static void _evloop_release_handler(vector<evloop_handler>::iterator &_Where)
+{
+	if ( (--_Where->refcount) == 0) {
+		CloseHandle(_Where->hEvent);
+		ctx.handlers.erase(_Where);
+	}
+}
+
+int evloop_init() {
+	InitializeCriticalSection(&ctx.critsec);
+	return 0;
+}
+
+static HANDLE _evloop_acquire_control_event()
+{
+	/* TODO: optimize */
+	return CreateEvent(NULL, FALSE, FALSE, NULL);
+}
+
+static void _evloop_release_control_event(HANDLE ev)
+{
+	/* TODO: optimize */
+	CloseHandle(ev);
+}
+
+static void _evloop_notifyall()
+{
+	HANDLE ev;
+
+	/* all waits after this are part of a new chain */
+	ev = InterlockedExchangePointer(&ctx.controlEvent, NULL);
+	if (ev) SetEvent(ev);
 }
