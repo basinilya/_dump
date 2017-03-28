333e8e91860921d72feccfff2aebb25cd06ace2b
Ilya Basin <basinilya@gmail.com>
2017-03-28 11:30:42 +0300

    x

--- src/main/java/org/bar/bgexecutor/BgExecutorThreadFactory.java
+++ src/main/java/org/bar/bgexecutor/BgExecutorThreadFactory.java
@@ -2,28 +2,37 @@ package org.bar.bgexecutor;
 
 import java.lang.ref.WeakReference;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
 
-public class BgExecutorThreadFactory implements ThreadFactory {
+class BgExecutorThreadFactory implements ThreadFactory {
     
-    private final WeakReference<BgExecutor> ctxRef;
+    /**
+     * {@link ThreadPoolExecutor} holds a strong reference to its thread factory. To allow gc
+     * collect {@link BgExecutor} instance we use a weak reference here
+     */
+    private final WeakReference<BgExecutor> exRef;
     
     public BgExecutorThreadFactory(final BgExecutor executor) {
-        this.ctxRef = new WeakReference<BgExecutor>(executor);
+        this.exRef = new WeakReference<BgExecutor>(executor);
     }
     
     @Override
     public Thread newThread(final Runnable r) {
-        return new ThreadWithTlsDestruction(r, ctxRef);
+        return new ThreadWithTlsDestruction(r, exRef);
     }
 }
 
 class ThreadWithTlsDestruction extends Thread {
     
-    private final WeakReference<BgExecutor> ctxRef;
+    /**
+     * Thread will not finish unless {@link BgExecutor} instance is collected and shutdown is
+     * called. To allow gc collect {@link BgExecutor} instance we use a weak reference here
+     */
+    private final WeakReference<BgExecutor> exRef;
     
-    public ThreadWithTlsDestruction(final Runnable r, final WeakReference<BgExecutor> ctxRef) {
+    public ThreadWithTlsDestruction(final Runnable r, final WeakReference<BgExecutor> exRef) {
         super(r);
-        this.ctxRef = ctxRef;
+        this.exRef = exRef;
     }
     
     @Override
@@ -31,10 +40,10 @@ class ThreadWithTlsDestruction extends Thread {
         try {
             super.run();
         } finally {
-            final BgExecutor ctx = ctxRef.get();
-            if (ctx != null) {
-                ctx.setThreadHint("dying");
-                ctx.destroyTls();
+            final BgExecutor executor = exRef.get();
+            if (executor != null) {
+                executor.setThreadHint("dying");
+                executor.destroyTls();
             }
         }
     }
