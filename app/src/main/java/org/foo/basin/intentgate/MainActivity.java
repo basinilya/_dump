5e0496898bd3615f372942e696861c43454e3117
Ilya Basin <basinilya@gmail.com>
2017-11-06 12:10:11 +0300

    x

--- app/src/main/java/org/foo/basin/intentgate/MainActivity.java
+++ app/src/main/java/org/foo/basin/intentgate/MainActivity.java
@@ -1,5 +1,6 @@
 package org.foo.basin.intentgate;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
@@ -8,12 +9,18 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
+import java.util.Date;
 import java.util.Map;
 
-public class MainActivity extends PreferenceActivity {
+public class MainActivity extends PreferenceActivity implements AdapterView.OnItemLongClickListener {
 
     private static final String TAG = "MainActivity";
+    private PreferenceCategory targetCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +28,20 @@ public class MainActivity extends PreferenceActivity {
         PreferenceManager prefMgr = getPreferenceManager();
         prefMgr.setSharedPreferencesName(getKnownIntentsPreferencesName());
         prefMgr.setSharedPreferencesMode(MODE_PRIVATE);
-        addPreferencesFromResource(R.xml.preferences);
+        addPreferencesFromResource(R.xml.prefs_known_intents);
+
         PreferenceScreen screen = getPreferenceScreen();
         int count = screen.getPreferenceCount();
         Log.d(TAG, "preference count: " + count);
-        for (int i = 0 ; i < count; i++) {
-            //
-            Preference preference = screen.getPreference(i);
-            Log.d(TAG, "preference class: " + preference.getClass().getSimpleName());
-            Log.d(TAG, "preference key: " + preference.getKey() );
-        }
-        PreferenceCategory targetCategory = (PreferenceCategory)findPreference("known_intents");
+
+        ListView listView = getListView();
+        listView.setOnItemLongClickListener(this);
+
+        targetCategory = (PreferenceCategory)findPreference("known_intents");
+
+        SharedPreferences.Editor editor = prefMgr.getSharedPreferences().edit();
+        editor.putBoolean(new Date().toString(), false);
+        editor.apply();
 
         Map<String, ?> knownIntents = prefMgr.getSharedPreferences().getAll();
         for (Map.Entry<String, ?> x : knownIntents.entrySet()) {
@@ -39,22 +49,28 @@ public class MainActivity extends PreferenceActivity {
             String key = x.getKey();
             checkBoxPreference.setKey(key);
             checkBoxPreference.setTitle(key);
+            // checkBoxPreference.getView(null, null).setOnLongClickListener();
             targetCategory.addPreference(checkBoxPreference);
         }
-
-/*
-*/
-        //Map<String, ?> knownIntents = PreferenceManager.getDefaultSharedPreferences(this).getAll();
-        //getSharedPreferences(getKnownIntentsPreferencesName(), MODE_PRIVATE);
-/*
-        for (Map.Entry<String, Boolean> x : knownIntents.entrySet()) {
-            adapter.add(x);
-        }
-        */
     }
 
     public String getKnownIntentsPreferencesName() {
-        return getPackageName() + "_knownIntents";
+        return getPackageName() + "_known_intents";
     }
 
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        ListView listView = (ListView) parent;
+        ListAdapter listAdapter = listView.getAdapter();
+        Object obj = listAdapter.getItem(position);
+        if (obj instanceof Preference) {
+            Preference pref = (Preference) obj;
+            targetCategory.removePreference(pref);
+            SharedPreferences.Editor editor = pref.getEditor();
+            editor.remove(pref.getKey());
+            editor.apply();
+            return true;
+        }
+        return false;
+    }
 }
